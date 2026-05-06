984c9d3dbb84a4d017a61addbc4bb5c9bd923744
ljlericson <ljlericson@protonmail.com>
2026-05-06 18:07:28 +1200

    Fix build worker script

--- build-worker.sh
+++ build-worker.sh
@@ -1,11 +1,18 @@
+#!/bin/sh
+set -e 
+
+ROOT_DIR="$(pwd)"
+
 if [ -d "worker/build" ]; then
   cd worker/build
-  make
-  cp TaskforgeWorker ../../bin/TaskForge-Worker
 else
   cd worker
   sh generate-makefile.sh
   cd build
-  make
-  cp TaskforgeWorker ../../bin/TaskForge-Worker
 fi
+
+make
+
+mkdir -p "$ROOT_DIR/bin"
+
+cp TaskforgeWorker "$ROOT_DIR/bin/TaskForge-Worker"
